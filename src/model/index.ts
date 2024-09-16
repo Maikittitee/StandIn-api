e57d0b7a4af8cc57a3af3554a32e7510e3cbe27e
Icarus <63220735+shoguncoffee@users.noreply.github.com>
2024-09-16 15:55:47 +0700

    change indentation

--- src/model/index.ts
+++ src/model/index.ts
@@ -1,18 +1,18 @@
 import { Schema, model } from 'mongoose';
 
 export interface IImage {
-  src: string;
-  alt: string;
+    src: string;
+    alt: string;
 }
 export const imageSchema = new Schema<IImage>({
-  src: { 
-    type: String, 
-    required: true 
-  },
-  alt: { 
-    type: String, 
-    required: true 
-  },
+    src: { 
+        type: String, 
+        required: true 
+    },
+    alt: { 
+        type: String, 
+        required: true 
+    },
 });
 export const Image = model<IImage>('Image', imageSchema);
 
--- src/model/product.ts
+++ src/model/product.ts
@@ -2,24 +2,24 @@ import { Schema, model } from 'mongoose';
 import { Image, IImage } from './index';
 
 export interface IProduct {
-  name: string;
-  image: string;
-  price: number;
+    name: string;
+    image: string;
+    price: number;
 }
 
 export const productSchema = new Schema<IProduct>({
-  name: { 
-    type: String, 
-    required: true 
-  },
-  image: { 
-    type: String, 
-    required: true 
-  },
-  price: {
-    type: Number,
-    required: true
-  }
+    name: { 
+        type: String, 
+        required: true 
+    },
+    image: { 
+        type: String, 
+        required: true 
+    },
+    price: {
+        type: Number,
+        required: true
+    }
 });
 
 export default model<IProduct>('Product', productSchema);
