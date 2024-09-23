27c503363122703c68c3a6a67d396f45bdaae848
ktunchar <maikittitee1615@gmail.com>
2024-09-23 21:54:10 +0700

    chore: connect to db

--- src/app.js
+++ src/app.js
@@ -1,6 +1,13 @@
+require('dotenv').config();
 const express = require('express')
 const app = express()
 const port = 3000
+const mongoose = require('mongoose')
+
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGO_URI)
+		.then(() => console.log("Mongo connected"))
+		.catch((error) => console.error(error))
 
 app.get('/', (req, res) => {
   res.send('Hello, Stand In!')
