191baa9f49512aed8f9c6d70164112536e1ab64f
Icarus <63220735+shoguncoffee@users.noreply.github.com>
2024-10-04 21:10:46 +0700

    update test db

--- src/test/db.ts
+++ src/test/db.ts
@@ -2,21 +2,20 @@ import 'dotenv/config';
 import mongoose from 'mongoose';
 
 import { Building, Store } from '../model/address.js';
-import { Order, OrderStatus } from '../model/order.js';
-import { TrackStatus, TaskType } from '../model/commission.js';
-import { Product, Brand } from '../model/product.js';
-import { QueueingTask, PackageSize } from '../model/queueing.js';
-import { ShoppingTask } from '../model/shopping.js';
+import { Order, OrderStatus, TrackStatus } from '../model/order.js';
+import { Brand, Product, ProductModel } from '../model/product.js';
+import { TaskType, PackageSize } from '../model/task.js';
+
 
 // @ts-expect-error
-mongoose.connect(process.env.ATLAS_URI);
+await mongoose.connect(process.env.ATLAS_URI);
 
 
 const siam_paragon = new Building({
     name: 'SIAM PARAGON',
     address: {
         country: 'Thailand',
-        postalcode: '10330',
+        zipcode: '10330',
         province: 'Bangkok',
         district: 'Pathum Wan',
         subdistrict: 'Pathum Wan',
@@ -34,96 +33,93 @@ const crafter = new Brand({
     logo: 'http://www.crafterguitars.com/eng/images/common/h_logo.png',
 });
 
-const best_guitar = new Product({
+
+// Crafter's guitar model
+const interesting_guitar = new ProductModel({
     name: 'TMC-035EQ',
     brand: crafter._id,
-    store: music_collection._id,
-    // category: undefined,
-    subproduct: [{
-        price: 19600,
-        available: true,
-        image: [
-            'https://images.squarespace-cdn.com/content/v1/549721b4e4b08be712d559be/1446476048437-8SI3ZPVYISJLFZW4VQ80/TM-1.jpg',
-        ],
-    }],
+    category: undefined,
+    variant: [
+        {
+            images: [
+                'https://images.squarespace-cdn.com/content/v1/549721b4e4b08be712d559be/1446476048437-8SI3ZPVYISJLFZW4VQ80/TM-1.jpg',
+            ],
+            price: 19600,
+            description: 'One of the best "Orchestra" body type guitars',
+            option: undefined,
+        },
+    ],
 });
 
-const guitar_task = new ShoppingTask({
+const guitar_variant = interesting_guitar.variant[0];
+
+// guitar selling at store
+const good_guitar = new Product({
     store: music_collection._id,
-    items: [{
-        product: best_guitar._id,
-        subproduct: 0,
-        quantity: 1,
+    model: interesting_guitar._id,
+    subproduct: [{
+        variant: guitar_variant._id,
+        available: true,
     }],
 });
 
 const guitar_order = new Order({
     task: {
-        type: TaskType.Shopping,
-        value: guitar_task._id,
+        kind: TaskType.Shopping,
+        store: music_collection._id,
+        items: [{
+            product: good_guitar._id,
+            variant: guitar_variant._id,
+            quantity: 1,
+        }],
     },
     orderStatus: OrderStatus.Paid,
     trackStatus: [
         {
-            dateTime: Date.now(),
+            datetime: Date.now(),
             status: TrackStatus.On_the_way
-        },
-        {
-            dateTime: Date.now(),
+        }, {
+            datetime: Date.now(),
             status: TrackStatus.Arrived_at_store
-        },
-        {
-            dateTime: Date.now(),
+        }, {
+            datetime: Date.now(),
             status: TrackStatus.Item_recieved
         },
     ],
-    // stander: '',
     review: {
         rating: 4,
-        comment: 'comment',
+        comment: 'some comment',
     },
-});
-
-const queue_task = new QueueingTask({
-    location: 'some location',
-    datetime: Date.now(),
-    size: PackageSize.Small,
-    detail: 'description',
+    // stander: '',
+    // customer: '',
 });
 
 const queue_order = new Order({
     task: {
-        type: TaskType.Queueing,
-        value: queue_task._id,
+        kind: TaskType.Queueing,
+        location: {
+            country: 'Thailand',
+            zipcode: '10330',
+            province: 'Bangkok',
+            district: 'Pathum Wan',
+            subdistrict: 'Pathum Wan',
+            detail: 'some where',
+        },
+        datetime: Date.now(),
+        size: PackageSize.Small,
+        detail: 'some description',
     },
     orderStatus: OrderStatus.Pending,
-    // trackStatus: undefined,
-    // stander: '',
     // review: undefined,
+    // trackStatus: [],
+    // stander: '',
+    // customer: '',
 });
 
-console.log('save');
-
-siam_paragon.save();
-console.log('save1');
-
-music_collection.save();
-console.log('save2');
-
-crafter.save();
-console.log('save3');
-
-best_guitar.save();
-console.log('save4');
-
-guitar_task.save();
-console.log('save5');
-
-guitar_order.save();
-console.log('save6');
-
-queue_task.save();
-console.log('save7');
-
-queue_order.save();
-console.log('save8');
+console.log(await siam_paragon.save());
+console.log(await music_collection.save());
+console.log(await crafter.save());
+console.log(await interesting_guitar.save());
+console.log(await good_guitar.save());
+console.log(await guitar_order.save());
+console.log(await queue_order.save());
